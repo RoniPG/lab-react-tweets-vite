480cf56fe6fcf420701fe6323f3f4acdb80a01ff
RoniPG <ronigmn@gmail.com>
2024-11-14 20:13:54 +0100

    Iteration 3 | Create the Components

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,3 +1,9 @@
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
 function Tweet({tweet}) {
 
   const {
@@ -8,33 +14,15 @@ function Tweet({tweet}) {
   
   return (
     <div className="tweet">
-      <img
-        src={image}
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={image}></ProfileImage>
 
       <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">{name}</span>
-            <span className="handle">{handle}</span>
-          </span>
-
-          <span className="timestamp">{timestamp}</span>
-        </div>
-
-        <p className="message">
-          {message}
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+      <div className="top">
+        <User name={name} handle={handle}></User>
+        <Timestamp timestamp={timestamp}></Timestamp>
+      </div>
+        <Message message={message}></Message>
+        <Actions></Actions>
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
